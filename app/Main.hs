a4202d7430a71a694f9f96d40352b8679a6c48ca
Matt Hunzinger <matthunz2@gmail.com>
2024-02-04 02:27:06 -0500

    Start a tauri window

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 import Foreign.C.String (CString, newCString)
+import Control.Monad
 
 foreign import ccall unsafe "c_hello" hello :: CString -> IO ()
 
 main :: IO ()
-main = do
+main =  forever $ do
     str <- newCString "World"
     hello str
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,56 @@
 use std::{ffi::CStr, os::raw::c_char};
 
+use tao::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+use wry::WebViewBuilder;
+
+fn main() -> wry::Result<()> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    #[cfg(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    ))]
+    let builder = WebViewBuilder::new(&window);
+
+    #[cfg(not(any(
+        target_os = "windows",
+        target_os = "macos",
+        target_os = "ios",
+        target_os = "android"
+    )))]
+    let builder = {
+        use tao::platform::unix::WindowExtUnix;
+        use wry::WebViewBuilderExtUnix;
+        let vbox = window.default_vbox().unwrap();
+        WebViewBuilder::new_gtk(vbox)
+    };
+
+    let _webview = builder.with_url("https://tauri.app")?.build()?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        if let Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } = event
+        {
+            *control_flow = ControlFlow::Exit
+        }
+    });
+}
+
 pub fn hello(input: &str) {
-    println!("Hello, {}!", input);
+    dbg!("Running!");
+    main().unwrap();
+    dbg!("Ran2!");
 }
 
 #[no_mangle]
